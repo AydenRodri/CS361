974adfcc3d17968a4c0dd595b5a0cec69b3b8dbc
AydenRodri <ar77203n@pace.edu>
2024-10-08 01:21:20 -0400

    Update InnerOuter.java

--- innerouter/InnerOuter.java
+++ innerouter/InnerOuter.java
@@ -3,31 +3,31 @@ package innerouter;
 public class InnerOuter {
 
     public static void main(String[] args) {
-        int result = outermostEvaluation(5);
+        int result = outermost(5);
         System.out.println("Result of outermost evaluation: " + result);
-        int innermostResult = innermostEvaluation(5);
+        int innermostResult = innermost(5);
         System.out.println("Result of innermost evaluation: " + innermostResult);
     }
     
-    public static int outermostEvaluation(int n) {
+    public static int outermost(int n) {
         System.out.println("Entering outermost with n = " + n);
         if (n <= 0) {
             System.out.println("Base case of outermost is: " + n);
             return 0;
         } else {
-            int value = n + outermostEvaluation(n - 1);
+            int value = n + outermost(n - 1);
             System.out.println("Return outermost with n = " + n + ", value = " + value);
             return value;
         }
     }
     
-    public static int innermostEvaluation(int n) {
+    public static int innermost(int n) {
         System.out.println("Entering innermost with n = " + n);
         if (n <= 0) {
             System.out.println("Base case of innermost: " + n);
             return 0;
         } else {
-            int value = innermostEvaluation(n - 1);
+            int value = innermost(n - 1);
             value += n;
             System.out.println("Returning innermost with n = " + n + ", value = " + value);
             return value;
